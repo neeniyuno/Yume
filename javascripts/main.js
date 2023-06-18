416dfac72c47957d2722cd3df121585c0c34de9d
neeniyuno <111344544+neeniyuno@users.noreply.github.com>
2023-06-18 13:45:35 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,7 +7,7 @@
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 5000,
+            autoplaySpeed: 3000,
         });
 
         $('.js-products-slider').slick({
